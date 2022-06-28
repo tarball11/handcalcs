381b6c83e8351f5e091101fc1cb4b909be05c54e
tarball11 <travis.carter@gmail.com>
2022-06-28 13:29:42 -0400

    Slight change to .onLoad()

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,8 @@
 .onLoad <- function(libname, pkgname) {
 	op <- options()
 
-	# Fetch the defaults
-	op.handcalcs<- handcalcs_defaults()
-
 	# Create the list with defaults if it does not exist
-	if(is.null(op$handcalcs)) options(handcalcs = op.handcalcs)
+	if(is.null(op$handcalcs)) options(handcalcs = handcalcs_defaults())
 
 	invisible()
 }
