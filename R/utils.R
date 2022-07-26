a023fe39a0403edc33ef940ae42d9ad7704bd7b6
tarball11 <travis.carter@gmail.com>
2022-07-26 10:41:37 -0400

    Added get_closest_x() to utils.R

--- R/utils.R
+++ R/utils.R
@@ -36,3 +36,12 @@ fmt<- function(x, digits = 4) {
 	sprintf(paste0('%1.', digits, 'f'), x)
 }
 
+# Returns the rows in the tbl that have values of x closest to the given vals
+get_closest_x <- function(tbl, val) {
+	l <- c()
+	for(i in 1:length(val)) {
+		l <- append(l, which.min(abs(tbl$x - val[i])))
+	}
+
+	dplyr::slice(tbl, l)
+}
